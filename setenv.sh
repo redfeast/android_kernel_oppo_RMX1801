879d26de7fdbc19913b3a6d5ba30094fbba31f03
redfeast <redfeastred@gmail.com>
2019-06-02 13:19:38 +0530

    configure to build with ccache

--- setenv.sh
+++ setenv.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
-export CROSS_COMPILE=~/1/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export PATH=/usr/lib/ccache:~/toolchains/gcc/aarch64-linux-android-4.9/bin:$PATH
+export CROSS_COMPILE=aarch64-linux-android-
 export ARCH=arm64 
 export SUBARCH=arm64
 export OPPO_TARGET_DEVICE=18321
 export KBUILD_BUILD_HOST=Ubuntu-18.04-bionic
 export KBUILD_BUILD_USER=redfeast😎
-export KERNELSRC=/home/redfeast/50G/linux
+export KERNELSRC=~/50G/linux
